08da0482018d6c858d72dde8d155b4188d2cdc31
gkh <gkhclass@163.com>
2022-07-12 20:51:41 +0800

    update2

--- src/components/RangePicker.ce.vue
+++ src/components/RangePicker.ce.vue
@@ -1,38 +1,39 @@
 <template>
-    <a-range-picker
-        @change="onRangeChange"
-        @ok="onRangeOk"
-        :allow-clear="_allowClear"
-        :autofocus="_autofocus"
-        :bordered="_bordered"
-        :date-render="_dateRender"
-        :disabled="_disabled"
-        :disabled-date="_disabledDate"
-        :dropdown-class-name="_dropdownClassName"
-        :get-popup-container="_getPopupContainer"
-        :input-read-only="_inputReadOnly"
-        :locale="_locale"
-        :mode="_mode"
-        :next-icon="_nextIcon"
-        :open="_open"
-        :placeholder="_placeholder"
-        :popup-style="_popupStyle"
-        :prev-icon="_prevIcon"
-        :size="_size"
-        :suffix-icon="_suffixIcon"
-        :super-next-icon="_superNextIcon"
-        :super-prev-icon="_superPrevIcon"
-        :value-format="_valueFormat"
-        :allow-empty="_allowEmpty"
-        :default-picker-value="_defaultPickerValue"
-        :disabled-time="_disabledTime"
-        :format="_format"
-        :ranges="_ranges"
-        :render-extra-footer="_renderExtraFooter"
-        :separator="_separator"
-        :show-time="_showTime"
-        :value="timeRange"
-    />
+  <a-range-picker
+      @change="onRangeChange"
+      @ok="onRangeOk"
+      @openChange="onOpenChange"
+      @calendarChange="onCalendarChange"
+      :allow-clear="_allowClear"
+      :autofocus="_autofocus"
+      :bordered="_bordered"
+      :date-render="_dateRender"
+      :disabled="_disabled"
+      :disabled-date="disabledDate"
+      :dropdown-class-name="_dropdownClassName"
+      :get-popup-container="_getPopupContainer"
+      :input-read-only="_inputReadOnly"
+      :locale="_locale"
+      :mode="_mode"
+      :next-icon="_nextIcon"
+      :open="_open"
+      :placeholder="_placeholder"
+      :popup-style="_popupStyle"
+      :prev-icon="_prevIcon"
+      :size="_size"
+      :super-next-icon="_superNextIcon"
+      :super-prev-icon="_superPrevIcon"
+      :value-format="_valueFormat"
+      :allow-empty="_allowEmpty"
+      :default-picker-value="_defaultPickerValue"
+      :disabled-time="_disabledTime"
+      :format="_format"
+      :ranges="_ranges"
+      :render-extra-footer="_renderExtraFooter"
+      :separator="_separator"
+      :show-time="_showTime"
+      :value="hackValue || timeRange"
+  />
 </template>
 <script>
 import { defineComponent, reactive, ref, watch } from 'vue';
@@ -77,18 +78,21 @@ export default defineComponent({
   props: propsStr,
   emits: ['onRangeChange', 'onRangeOk'],
   setup(props, {emit}) {
-    console.log(props, 77777777777)
     const parseProps = reactive({})
-    const timeRange = ref([])
+    const timeRange = ref()
+    const dates = ref();
+    const hackValue = ref();
 
     propsStr.forEach(v => {
-      if (props[v]) {
+      if (props[v] !== undefined) {
         if (v === 'value') {
           const tr = JSON.parse(props.value)
           timeRange.value = [dayjs(tr[0]), dayjs(tr[1])]
         } else {
-          parseProps[`_${v}`] = ref(JSON.parse(props[v]))
+          parseProps[`_${v}`] = JSON.parse(props[v])
         }
+      } else {
+        parseProps[`_${v}`] = undefined
       }
     })
 
@@ -101,6 +105,34 @@ export default defineComponent({
         {deep: true}
     )
 
+    const disabledDate = current => {
+      if (!Number.isNaN(Number(parseProps._disabledDate))) {
+        if (!dates.value || dates.value.length === 0) {
+          return false;
+        }
+        const tooLate = dates.value[0] && current.diff(dates.value[0], 'days') > parseProps._disabledDate;
+        const tooEarly = dates.value[1] && dates.value[1].diff(current, 'days') > parseProps._disabledDate;
+        return tooEarly || tooLate;
+      } else if (Array.isArray(parseProps._disabledDate)) {
+
+        return current && (current > dayjs(parseProps._disabledDate[1]).endOf('day') ||
+            current < dayjs(parseProps._disabledDate[0]).startOf('day'))
+      }
+    };
+
+    let onOpenChange = open => {
+      if (open) {
+        dates.value = [];
+        hackValue.value = [];
+      } else {
+        hackValue.value = undefined;
+      }
+    };
+
+    const onCalendarChange = val => {
+      dates.value = val;
+    };
+
     const onRangeChange = (value, dateString) => {
       emit('onRangeChange', value || [], dateString)
       emit('update:value', value || [])
@@ -110,13 +142,16 @@ export default defineComponent({
       emit('onRangeOk', value)
     };
 
-
-    console.log(parseProps, 999)
     return {
       onRangeChange,
       onRangeOk,
       ...parseProps,
-      timeRange
+      timeRange,
+      dates,
+      hackValue,
+      disabledDate,
+      onOpenChange,
+      onCalendarChange
     };
   },
 });
